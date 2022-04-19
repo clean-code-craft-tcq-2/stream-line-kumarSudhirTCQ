168b0311abce3377316695972193f8e04a84da4e
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-04-19 20:54:58 +0530

    Update and rename Receiver.cpp to Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -39,13 +39,10 @@ void fillSensorValuesMap(T_SensorValues& sensorValues)
     float sum = 0.0;
     for(int i = 0 ; i < sensorValues.size(); ++i)
     {
-        ::cout<<"sensorValues["<<i<<"] - "<<sensorValues[i]<<" "<<"sum(before) - "<<sum<<" ";
         sum += sensorValues[i];
         float average = sum/(i+1);
-        ::cout<<"sum(after) - "<<sum<<" "<<"average - "<<average<<endl;
         smaValuesList.push_back(average);
     }
-    ::cout<<"smaValuesList size - "<<smaValuesList.size()<<endl;
     return smaValuesList;
 }
 
@@ -57,43 +54,3 @@ void processSMA()
         m_smaValues[sensoraluesIt->first] = calucateSMA(sensoraluesIt->second);
     }
 }
-
-void printSenorValues()
-{
-    ::std::map<::string, ::vector<int>>::const_iterator sensoraluesIt = m_sensorValues.begin();
-    for(;sensoraluesIt != m_sensorValues.end(); ++sensoraluesIt)
-    {
-        ::cout<<sensoraluesIt->first<<" ";
-        ::vector<int> readings = sensoraluesIt->second;
-        ::cout<<readings.size()<<endl;
-        for(int i = 0; i<readings.size(); ++i)
-        {
-            ::cout<<readings[i]<<" ";
-        }
-        ::cout<<endl;
-    }
-}
-
-void printSMAValues(::std::vector< float> smaValuesList)
-{
-    for(int i = 0; i<smaValuesList.size(); ++i)
-    {
-        ::cout<<smaValuesList[i]<<" ";
-    }
-}
-
-// int main() 
-// {
-//     // Write C++ code here
-//     fillSensorValuesMap(m_sensorValues);
-//     printSenorValues();
-//     processSMA();
-//     T_SMAValues::iterator smaValuesIt = m_smaValues.begin();
-//     for(;smaValuesIt != m_smaValues.end(); ++smaValuesIt)
-//     {
-//         ::cout<<smaValuesIt->first<<endl;
-//         printSMAValues(smaValuesIt->second);
-//         ::cout<<endl;
-//     }
-//     return 0;
-// }
