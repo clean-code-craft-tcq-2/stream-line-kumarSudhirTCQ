58aea72349f4c37ff0045f8c038ad95bd1b38d78
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-05-12 00:14:59 +0530

    Update TestReceiver.cpp

--- TestReceiver.cpp
+++ TestReceiver.cpp
@@ -6,8 +6,47 @@
 
 using namespace std;
 
-TEST_CASE("Check_for_Read_data_from_console")
+TEST_CASE("check_for_Receive_And_Compute_Sensor_data")
 {
-  readTheDataFromConsole();
+ T_SensorDataMap sensorDataMap = receiveAndComputeTheSensorData(&readAndStoreTheDataFromConsole,
+                                                                &calculateSMAforLastFewValues, 
+                                                                &calculateMaxAndMinValuesForGivenSensor, 
+                                                                &printDataForGivenSensor);
+  REQUIRE(sensorDataMap.size() == 2);
+  for(unisigned int i = 0; i < sensorDataMap.size(); ++i)
+  {
+    SensorData sensorData = sensorDataMap[i];
+    REQUIRE(sensorData.smaValue != 0.0);
+  }
 }
 
+TEST_CASE("check_for_calculate_SMA_value_for_given_Sensor_Readings")
+{
+  ::std::vector<int> sensorReadings = {1,8,5,6,8,5,2,8,7,4,16,58,74,23,6,8,52,2,1,5,2,65,8};
+  float expectedSmaValue = 16.2;
+  float actualSmaValue = calculateSMAforLastFewValues(sensorReadings);
+  REQUIRE(actualSmaValue == expectedSmaValue)
+}
+
+TEST_CASE("check_for_find_Max_Min_values_For_given_Readings")
+{
+  MaxAndMinValues minMaxValue;
+  ::std::vector<int> sensorReadings = {1,8,5,6,8,5,2,8,7,4,16,58,74,23,6,8,52,2,1,5,2,65,8};
+  int expectedMaxValue = 74;
+  int expectedMinValue = 1;
+  actualMinMaxValue = findMaxAndMinValuesForGivenSensor(sensorReadings);
+  REQUIRE(actualMinMaxValue.minValue == expectedMinValue);
+  REQUIRE(actualMinMaxValue.maxValue == expectedMaxValue);
+}
+
+TEST_CASE("check_for_read_data_from_console_And_store_the_Data")
+{
+  T_SensorValues sensorValuesMap;
+  readAndStoreTheDataFromConsole(sensorValuesMap);
+  REUIRE(sensorValuesMap.size() == 2)
+  for(unisigned int i = 0; i < sensorValuesMap.size(); ++i)
+  {
+    ::std::vector<int> sensorReadings = sensorValuesMap[i];
+    REQUIRE(sensorReadings.size() == 50)
+  }
+}
